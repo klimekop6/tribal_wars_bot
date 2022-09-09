a40d7ae8d660e11deaa75c368636137f5dfd8795
klimekop6 <91018442+klimekop6@users.noreply.github.com>
2022-09-09 19:33:24 +0200

    Revert last commit

---
+++ client_config.py
@@ -0,0 +1,10 @@
+class ClientConfig(object):
+    PUBLIC_KEY = "R5v6Jw2gRMm/ZfJF5KyAffHfiPupp2BW5wpc836rNQ8"
+    APP_NAME = "TribalWarsBot"
+    COMPANY_NAME = "k.spec"
+    HTTP_TIMEOUT = 5
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = [
+        "https://kspec.eu.pythonanywhere.com/tribalwarsbot/download/",
+        "http://likeapro.ddns.net:55557/FTP/",
+    ]
